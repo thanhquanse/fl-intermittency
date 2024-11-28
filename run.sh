efb1fbe9f02880f76bf9f1efe7ce45b2f2a383f1
Nguyen Thanh Quan <ntquan.sdh20@hcmut.edu.vn>
2024-11-28 15:09:24 +0700

    5th commit to separate common functions

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 # notebook_arr="FL-DT-full-client-updates-weather.ipynb FL-DT-no-updates-ignored-10x10-01-noadjacency-weather.ipynb"
-notebook_arr="FL-DT-full-client-updates-electricity.ipynb FL-DT-full-client-updates-traffic.ipynb FL-DT-full-client-updates-psm.ipynb"
-# notebook_arr="FL-DT-no-updates-ignored-10x10-01-noadjacency-electricity.ipynb FL-DT-no-updates-ignored-10x10-01-noadjacency-weather.ipynb FL-DT-no-updates-ignored-10x10-01-noadjacency-traffic.ipynb FL-DT-no-updates-ignored-10x10-01-noadjacency-psm.ipynb"
+# notebook_arr="FL-DT-full-client-updates-electricity.ipynb FL-DT-full-client-updates-traffic.ipynb FL-DT-full-client-updates-psm.ipynb FL-DT-full-client-updates-weather.ipynb"
+notebook_arr="FL-DT-no-updates-ignored-10x10-01-noadjacency-electricity.ipynb FL-DT-no-updates-ignored-10x10-01-noadjacency-weather.ipynb FL-DT-no-updates-ignored-10x10-01-noadjacency-traffic.ipynb FL-DT-no-updates-ignored-10x10-01-noadjacency-psm.ipynb"
+# notebook_arr="FL-DT-no-updates-ignored-10x10-02-2adjacency-electricity.ipynb FL-DT-no-updates-ignored-10x10-02-2adjacency-weather.ipynb FL-DT-no-updates-ignored-10x10-02-2adjacency-traffic.ipynb FL-DT-no-updates-ignored-10x10-02-2adjacency-psm.ipynb"
+# notebook_arr="FL-DT-proposed-method-10x10-01-noadjacency-electricity.ipynb FL-DT-proposed-method-10x10-01-noadjacency-weather.ipynb FL-DT-proposed-method-10x10-01-noadjacency-traffic.ipynb FL-DT-proposed-method-10x10-01-noadjacency-psm.ipynb"
+
 for notebook in $notebook_arr; do
     echo "Processing $notebook..."
     jupyter nbconvert --execute --to notebook --allow-errors $notebook
